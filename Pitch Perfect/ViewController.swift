58c7409e270326c4f92a59becd7cc1b2d5deb208
Abbas Aniefa <abbasaniefa@ivy-dev1-d013.local>
2015-12-22 09:00:38 +0530

    Initial Commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Abbas Aniefa on 22/12/15.
+//  Copyright © 2015 Abbas Aniefa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pitch PerfectTests/Pitch_PerfectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pitch_PerfectTests.swift
+//  Pitch PerfectTests
+//
+//  Created by Abbas Aniefa on 22/12/15.
+//  Copyright © 2015 Abbas Aniefa. All rights reserved.
+//
+
+import XCTest
+@testable import Pitch_Perfect
+
+class Pitch_PerfectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
