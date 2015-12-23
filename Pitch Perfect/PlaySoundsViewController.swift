d50c18228761c6ff84d3ce9cd4d630a7a574ff90
Abbas Aniefa <abbasaniefa@ivy-dev1-d013.local>
2015-12-23 10:34:50 +0530

    Navigation controller added to switch between views.

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Abbas Aniefa on 23/12/15.
+//  Copyright © 2015 Abbas Aniefa. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playslow(sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,24 +11,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden=true;
+    }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
 
 
     @IBAction func stopRecording(sender: UIButton) {
-        recordingInProgress.hidden=true
+        recordingInProgress.hidden=true;
+        recordButton.enabled=true;
+        stopButton.hidden=true;
     }
     
     @IBAction func recordbutton(sender: UIButton) {
-        
-        recordingInProgress.hidden=false
+        recordButton.enabled=false;
+        stopButton.hidden=false;
+        recordingInProgress.hidden=false;
         //TODO:record the voice
         print("record button clicked");
     }
